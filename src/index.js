7c9ea5c003a9544b54e168401698bb1d9640e6cb
Andrew Yudin <pasarian@gmail.com>
2016-11-25 12:59:13 +0600

    Add Stripe service

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import braintree_service from './providers/braintree';
 import pci_proxy_service from './providers/pci_proxy';
+import stripe_service from './providers/stripe';
 
 const SERVICES = {
   braintree_service,
-  pci_proxy_service
+  pci_proxy_service,
+  stripe_service
 }
 
 export function init(service, settings) {
