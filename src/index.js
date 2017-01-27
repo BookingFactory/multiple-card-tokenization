a630e00bf76530dda29de871b1d6bf0591331034
Kirill Ivanov <k.ivanov@speople.pro>
2017-01-27 12:59:42 +0600

    Add omise tokenization

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import braintree_service from './providers/braintree';
 import pci_proxy_service from './providers/pci_proxy';
 import stripe_service from './providers/stripe';
+import omise_service from './providers/omise';
 
 const SERVICES = {
   braintree_service,
   pci_proxy_service,
-  stripe_service
+  stripe_service,
+  omise_service
 }
 
 export function init(service, settings) {
