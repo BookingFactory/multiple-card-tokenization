305b1166c26dfd1c1778cc18c2a2c8aa0298919a
Marko Tomic <markot@origo.is>
2022-09-07 14:29:40 +0200

    update webpack from v1 to v3

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,25 @@ var webpack = require('webpack');
 module.exports = {
   entry: './src/index',
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        loader: 'babel',
         exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'stage-1']
-        }
+        use: [
+          { loader: 'babel-loader' }
+        ]
       }
     ]
+    // loaders: [
+    //   {
+    //     test: /\.js$/,
+    //     loader: 'babel',
+    //     exclude: /node_modules/,
+    //     query: {
+    //       presets: ['es2015', 'stage-1']
+    //     }
+    //   }
+    // ]
   },
   output: {
     filename: 'dist/multipleCardTokenization.min.js',
@@ -20,7 +29,7 @@ module.exports = {
     library: 'multipleCardTokenization'
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
+    // new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production'),
